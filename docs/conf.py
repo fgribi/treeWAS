a89e3976aeb61101f85e17e1802e2295faf765be
Fabian Gribi <gribi.fabian@gmail.com>
2024-12-23 15:06:28 +0100

    Add sphinx documentation structure

---
+++ docs/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'treeWAS'
+copyright = '2024, Fabian Gribi'
+author = 'Fabian Gribi'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../treewas'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc"
+]
+autodoc_default_options = {
+    'members': True,
+    'special-members': '__init__',
+    'inherited-members': True,
+    'show-inheritance': True,
+}
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
